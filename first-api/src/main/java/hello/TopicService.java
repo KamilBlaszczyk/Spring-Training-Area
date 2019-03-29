de7c10c5093082dceeb9dd84da7394bc68a1bdfc
Kamil Blaszczyk <blaszczyk.kamil.piotr@gmail.com>
2019-03-29 23:37:37 +0100

    Create TopicService.java

---
+++ first-api/src/main/java/hello/TopicService.java
@@ -0,0 +1,44 @@
+package hello;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+                new Topic("1", "name1", "Topic 1"),
+                new Topic("2", "name2", "Topic 2"),
+                new Topic("3", "name3", "Topic 3")
+        ));
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++){
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)){
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+    }
+
+}
