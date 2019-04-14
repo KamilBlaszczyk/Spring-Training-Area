40c611668b3579b0c9bc09029a32bec62dff04fb
Kamil Blaszczyk <blaszczyk.kamil.piotr@gmail.com>
2019-04-14 23:16:44 +0100

    Create CourseDto.java

---
+++ courseData/src/main/java/com/example/courseData/Course/CourseDto.java
@@ -0,0 +1,13 @@
+package com.example.courseData.Course;
+
+import com.example.courseData.Topics.Topic;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CourseDto {
+    Topic topicId;
+
+
+}
