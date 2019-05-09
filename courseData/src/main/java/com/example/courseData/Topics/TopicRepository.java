9a3666492477468708c812eb4cc17873ca248101
Kamil Blaszczyk <blaszczyk.kamil.piotr@gmail.com>
2019-05-09 21:38:14 +0200

    Delete TopicRepository.java

--- courseData/src/main/java/com/example/courseData/Topics/TopicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.courseData.Topics;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, Integer> {
-    Topic getTopicById(Integer id);
-
-}
